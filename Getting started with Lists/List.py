2491ec1a7c958c8fc64ac209359a693cfc209705
adhiraj-code <alkarathore87@gmail.com>
2026-04-27 19:45:19 +0530

    Add files via upload

---
+++ Getting started with Lists/List.py
@@ -0,0 +1,20 @@
+list1 = [23, 46, 98, 10, 34]
+
+print(len(list1))
+
+print(list1[2])
+
+list1.append(78)
+print(list1)
+
+list1.insert(0, 14)
+print(list1)
+
+list1.pop()
+print(list1)
+
+list1.reverse()
+print(list1)
+
+list1.sort()
+print(list1)
---
+++ Getting started with Lists/Word Matching.py
@@ -0,0 +1,6 @@
+list1 = ["apple", "banana", "kiwi", "pear", "grapes"]
+
+if "apple" in list1:
+    print("String Found")
+else:
+    print("String Not Found")
