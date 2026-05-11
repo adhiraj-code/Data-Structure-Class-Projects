757c7aab10d7c8b2bf154cae5f2bcf6ba2eb58d3
adhiraj-code <alkarathore87@gmail.com>
2026-05-11 19:56:23 +0530

    Add files via upload

---
+++ Sets and Arrays/Operations in Sets.py
@@ -0,0 +1,15 @@
+set1 = {1, 2, 3, 4, 5}
+print(set1)
+
+set2 = {"Adhiraj", 6, 11, 35,(1,2,3)}
+print(set2)
+
+set3 = {0,1,1,2,3,5}
+print(set3)
+
+set4 = {1,2,3,4,5}
+list1 = list(set4)
+print(list1)
+
+list1.pop()
+print(list1)
---
+++ Sets and Arrays/Set Intersection.py
@@ -0,0 +1,15 @@
+set1 = {1,2,3,4,5}
+
+set2 = {5,6,7,8,9}
+
+set3 = set1.union(set2)
+
+print(set3)
+
+set4 = set1.intersection(set2)
+
+print(set4)
+
+set5 = set1.symmetric_difference(set2)
+
+print(set5)
