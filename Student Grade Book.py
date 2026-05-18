be83eef07a111674698b4b14c50699d00331af8b
adhiraj-code <alkarathore87@gmail.com>
2026-05-18 19:50:57 +0530

    Add files via upload

---
+++ Student Grade Book.py
@@ -0,0 +1,17 @@
+dict1 = {"Adhiraj":89, "Aditya":91, "Rohan":78, "Ananya":87, "R":95}
+
+x = dict1.values()
+print(x)
+
+total = sum(x)
+print(total)
+
+average = total / 5
+print(average)
+
+maximum = max(x)
+print(maximum)
+
+minimum = min(x)
+print(minimum)
+
